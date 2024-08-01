533547360f0ec6343f1e042f5ea0d84f3a297bd5
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-07-31 22:38:26 -0400

    feat: DNA strand on page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,13 @@
+import DNA from "../components/UI/DNA";
+import "../styles/dna.css";
+
 export default function About() {
-  return <h1>Hi! I'm Gabby</h1>;
+  return (
+    <div>
+      <div className="dna">
+        <DNA />
+      </div>
+      <h1>Hi! I'm Gabby</h1>
+    </div>
+  );
 }
