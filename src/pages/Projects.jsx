ea861f93ffd906203f32780f63bcab6d38f53078
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-08-19 12:18:04 -0400

    feat: change background and text color

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,17 +1,5 @@
 /* DEPENDENCIES */
 import { useRef, useEffect, useState } from "react";
-// import Video from "../components/Video";
-
-// style={{
-//   cursor: "auto",
-//   width: "100%",
-//   height: "100%",
-//   borderRadius: "0px",
-//   display: "block",
-//   objectFit: "cover",
-//   backgroundColor: "rgba(0, 0, 0, 0)",
-//   objectPosition: "50% 50%",
-// }}
 
 /* EXPORT */
 export default function Projects() {
